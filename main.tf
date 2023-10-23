8e8ceb97a4301c156e959bcd8d6e6333b276c299
sandeepcheekuri <96152133+sandeepcheekuri@users.noreply.github.com>
2023-10-23 10:05:10 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIAY6GGA6VF3KYT2KMZ"
+    secret_key = "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"
+  
+}
+
+resource "aws_instance" "web" {
+    ami = "ami-0df435f331839b2d6"
+    instance_type = "t2.micro"
+    availability_zone = "us-easy-1a"
+    key_name = "jenkins-server-key"
+  
+  tags = {
+    Name ="Web_server"
+  }
+}
